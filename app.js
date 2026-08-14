fd9395760e8ecdaad376ff6e97fcc11969cfa683
manaswasnotguilty69-jpg <manaswasnotguilty69@gmail.com>
2026-08-14 14:59:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const codes = [
   { code: "9ET48BS5VUR9ZSNN", expires: "₹300 Reward" },
   { code: "DTWU191YLR89ZDBB", expires: "₹400 Reward" },
   { code: "H3GUDCKTYBRYBEFF", expires: "₹500 Reward" },
-  { code: "A5K2MGULGFE42W0A", expires: "₹600 Reward" },
+  { code: "A5K2MGULGFE42WOA", expires: "₹600 Reward" },
   { code: "4C4SLLTK4LBJKNEU", expires: "₹700 Reward" },
   { code: "FA5S6M4YX8R3U8P3", expires: "₹800 Reward" },
   { code: "08BH2ELABBBU8RD8", expires: "₹900 Reward" },
